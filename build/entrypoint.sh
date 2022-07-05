5200b5dbec49a1a6ebd00de61663f22ed03a581d
Tropicar <zerozept@protonmail.com>
2022-07-05 18:48:05 +0200

    Use entrypoint file and set up override totaldifficulty

---
+++ build/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -n $OVERRIDE_TTD ]] && [[ $EXTRA_OPTS != *"--override.terminaltotaldifficulty"* ]]; then
+  EXTRA_OPTS="--override.terminaltotaldifficulty=${OVERRIDE_TTD} ${EXTRA_OPTS}"
+fi
+
+exec -c geth --datadir /sepolia \ 
+    --sepolia \
+    --http \
+    --http.addr 0.0.0.0 \
+    --http.corsdomain "*" \
+    --http.vhosts "*" \
+    --ws \
+    --ws.origins "*" \
+    --ws.addr 0.0.0.0 \
+    --authrpc.addr 0.0.0.0 \
+    --authrpc.port 8551 \
+    --authrpc.vhosts "*" \
+    --authrpc.jwtsecret "/jwtsecret" \
+    --syncmode ${SYNCMODE:-snap} \ 
+    --metrics \
+    --metrics.addr 0.0.0.0 \
+    $EXTRA_OPTIONS
